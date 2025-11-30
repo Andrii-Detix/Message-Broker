bb28b0d6cba5642d125574b83c86f82196b48f66
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-11-30 15:42:37 +0200

    Implement WriteAheadLogManager orchestration
    - Implement WriteAheadLogManager class to route wal events to corresponding file appenders;
    - Add unit tests using mocks to verify routing correctness and exception handling;

---
+++ MessageBroker.Persistence/WriteAheadLogs/WriteAheadLogManager.cs
@@ -0,0 +1,67 @@
+﻿using MessageBroker.Persistence.Abstractions;
+using MessageBroker.Persistence.Events;
+using MessageBroker.Persistence.WriteAheadLogs.Exceptions;
+using Microsoft.Extensions.Logging;
+
+namespace MessageBroker.Persistence.WriteAheadLogs;
+
+public class WriteAheadLogManager : IWriteAheadLog
+{
+    private readonly IFileAppender<EnqueueWalEvent> _enqueueAppender;
+    private readonly IFileAppender<AckWalEvent> _ackAppender;
+    private readonly IFileAppender<DeadWalEvent> _deadAppender;
+
+    private readonly ILogger<WriteAheadLogManager>? _logger;
+
+    public WriteAheadLogManager(
+        IFileAppender<EnqueueWalEvent>? enqueueAppender,
+        IFileAppender<AckWalEvent>? ackAppender,
+        IFileAppender<DeadWalEvent>? deadAppender,
+        ILogger<WriteAheadLogManager>? logger = null)
+    {
+        ArgumentNullException.ThrowIfNull(enqueueAppender);
+        ArgumentNullException.ThrowIfNull(ackAppender);
+        ArgumentNullException.ThrowIfNull(deadAppender);
+        
+        _enqueueAppender = enqueueAppender;
+        _ackAppender = ackAppender;
+        _deadAppender = deadAppender;
+        _logger = logger;
+    }
+    
+    public bool Append(WalEvent evt)
+    {
+        try
+        {
+            DispatchWalEvent(evt);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger?.LogError(ex, $"Storage of event '{evt.GetType().Name}' completed with failure '{ex.Message}'.");
+            
+            return false;
+        }
+    }
+
+    private void DispatchWalEvent(WalEvent evt)
+    {
+        switch (evt)
+        {
+            case EnqueueWalEvent e:
+                _enqueueAppender.Append(e);
+                break;
+            
+            case AckWalEvent a:
+                _ackAppender.Append(a);
+                break;
+            
+            case DeadWalEvent d:
+                _deadAppender.Append(d);
+                break;
+            
+            default:
+                throw new WalEventUnknownTypeException();
+        }
+    }
+}
