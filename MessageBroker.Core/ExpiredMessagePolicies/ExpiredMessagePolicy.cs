c093fa8c041901bfd1436c25ab61874e9dedf5d3
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-02 19:22:50 +0200

    Implement ExpiredMessagePolicy class
    - Implement ExpiredMessagePolicy class to verify whether sent message is expired;
    - Add unit tests to verify logic correctness;

---
+++ MessageBroker.Core/ExpiredMessagePolicies/ExpiredMessagePolicy.cs
@@ -0,0 +1,43 @@
+﻿using MessageBroker.Core.Abstractions;
+using MessageBroker.Core.ExpiredMessagePolicies.Exceptions;
+using MessageBroker.Core.Messages.Models;
+
+namespace MessageBroker.Core.ExpiredMessagePolicies;
+
+public class ExpiredMessagePolicy : IExpiredMessagePolicy
+{
+    private readonly TimeSpan _expirationTime;
+    private readonly TimeProvider _timeProvider;
+
+    public ExpiredMessagePolicy(
+        TimeSpan expirationTime, 
+        TimeProvider? timeProvider)
+    {
+        ArgumentNullException.ThrowIfNull(timeProvider);
+
+        if (expirationTime <= TimeSpan.Zero)
+        {
+            throw new MessageExpirationTimeInvalidException();
+        }
+        
+        _expirationTime = expirationTime;
+        _timeProvider = timeProvider;
+    }
+    
+    public bool IsExpired(Message message)
+    {
+        ArgumentNullException.ThrowIfNull(message);
+        
+        if (message.State != MessageState.Sent 
+            || message.LastSentAt is null)
+        {
+            return false;
+        }
+        
+        DateTimeOffset now = _timeProvider.GetUtcNow();
+
+        TimeSpan diff = now - message.LastSentAt.Value;
+
+        return diff >= _expirationTime;
+    }
+} 
