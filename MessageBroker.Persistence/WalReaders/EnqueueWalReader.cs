06b8f85b5fb2298f9bb0946b7c9e5e601ccf5af5
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-05 22:29:52 +0200

    Implement EnqueueWalReader
    - Implement EnqueueWalReader to deserialize binary enqueue wal events;
    - Add integration tests verifying logic correctness;

---
+++ MessageBroker.Persistence/WalReaders/EnqueueWalReader.cs
@@ -0,0 +1,41 @@
+﻿using MessageBroker.Persistence.Events;
+
+namespace MessageBroker.Persistence.WalReaders;
+
+public class EnqueueWalReader : AbstractWalReader<EnqueueWalEvent>
+{
+    protected override bool TryReadNext(BinaryReader reader, out EnqueueWalEvent? evt)
+    {
+        const int intSize = 4;
+        const int guidSize = 16;
+        
+        evt = null;
+        
+        // Read stored event length
+        if (!CanRead(reader.BaseStream, intSize))
+        {
+            return false;
+        }
+
+        int length = reader.ReadInt32();
+        int restLength = length - intSize;
+
+        // Read rest of stored event
+        if (!CanRead(reader.BaseStream, restLength))
+        {
+            return false;
+        }
+
+        WalEventType type = (WalEventType)reader.ReadInt32();
+
+        byte[] buffer = reader.ReadBytes(guidSize);
+        Guid messageId = new Guid(buffer);
+
+        int payloadLength = restLength - intSize - guidSize;
+        buffer = reader.ReadBytes(payloadLength);
+
+        evt = new EnqueueWalEvent(messageId, buffer);
+        
+        return true;
+    }
+}
