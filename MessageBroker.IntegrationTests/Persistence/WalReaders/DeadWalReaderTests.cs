c6cadadc0ccc39d5253ea5463c4b407deed28520
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-05 22:54:27 +0200

    Implement DeadWalReader
    - Implement DeadWalReader to deserialize binary dead wal events;
    - Add integration tests verifying logic correctness;

---
+++ MessageBroker.IntegrationTests/Persistence/WalReaders/DeadWalReaderTests.cs
@@ -0,0 +1,100 @@
+﻿using MessageBroker.Persistence.Events;
+using MessageBroker.Persistence.WalReaders;
+using Shouldly;
+
+namespace MessageBroker.IntegrationTests.Persistence.WalReaders;
+
+public class DeadWalReaderTests : IDisposable
+{
+        private readonly string _directory;
+
+    public DeadWalReaderTests()
+    {
+        _directory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        Directory.CreateDirectory(_directory);
+    }
+    
+    [Fact]
+    public void Read_ReturnsEventWithPayload_WhenFileFormatIsCorrect()
+    {
+        // Arrange
+        string filePath = Path.Combine(_directory, "valid.log");
+        Guid messageId = Guid.CreateVersion7();
+
+        using (var writer = new BinaryWriter(File.OpenWrite(filePath)))
+        {
+            writer.Write(messageId.ToByteArray());
+        }
+        
+        DeadWalReader sut = new();
+
+        // Act
+        DeadWalEvent[] actual = sut.Read(filePath).ToArray();
+
+        // Assert
+        actual.Length.ShouldBe(1);
+        DeadWalEvent evt = actual.First();
+        
+        evt.MessageId.ShouldBe(messageId);
+    }
+    
+    [Fact]
+    public void Read_IgnoresRecord_WhenFileIsCorrupted()
+    {
+        // Arrange
+        string filePath = Path.Combine(_directory, "valid.log");
+        Guid messageId = Guid.CreateVersion7();
+        
+        byte[] data = messageId
+            .ToByteArray()
+            .Take(14)
+            .ToArray();
+
+        using (var writer = new BinaryWriter(File.OpenWrite(filePath)))
+        {
+            writer.Write(data);
+        }
+        
+        DeadWalReader sut = new();
+
+        // Act
+        DeadWalEvent[] actual = sut.Read(filePath).ToArray();
+
+        // Assert
+        actual.ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void Read_CanReadMultipleEvents_FromSingleFile()
+    {
+        // Arrange
+        string filePath = Path.Combine(_directory, "valid.log");
+        Guid messageId1 = Guid.CreateVersion7();
+        Guid messageId2 = Guid.CreateVersion7();
+        
+        using (var writer = new BinaryWriter(File.OpenWrite(filePath)))
+        {
+            writer.Write(messageId1.ToByteArray());
+            writer.Write(messageId2.ToByteArray());
+        }
+        
+        DeadWalReader sut = new();
+
+        // Act
+        DeadWalEvent[] actual = sut.Read(filePath).ToArray();
+        
+        // Assert
+        actual.Length.ShouldBe(2);
+        
+        actual[0].MessageId.ShouldBe(messageId1);
+        actual[1].MessageId.ShouldBe(messageId2);
+    }
+
+    public void Dispose()
+    {
+        if (Directory.Exists(_directory))
+        {
+            Directory.Delete(_directory, true);
+        }
+    }
+}
