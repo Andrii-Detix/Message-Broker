72790805d7519d1d821c2de419b7ee9594278e4e
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-05 17:27:46 +0200

    Add IWalReader interface to read wal events from file

---
+++ MessageBroker.Persistence/Abstractions/IWalReader.cs
@@ -0,0 +1,9 @@
+﻿using MessageBroker.Persistence.Events;
+
+namespace MessageBroker.Persistence.Abstractions;
+
+public interface IWalReader<out TEvent>
+    where TEvent : WalEvent
+{
+    IEnumerable<TEvent> Read(string filePath);
+}
