3297a99d4eacb425e660c3dfc48f5916182d5ac3
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-06 15:16:31 +0200

    Add WalManifest and WalFiles records

---
+++ MessageBroker.Persistence/Manifests/WalManifest.cs
@@ -0,0 +1,12 @@
+﻿namespace MessageBroker.Persistence.Manifests;
+
+public record WalManifest
+{
+    public string Enqueue { get; init; } = string.Empty;
+    
+    public string Ack { get; init; } = string.Empty;
+    
+    public string Dead { get; init; } = string.Empty;
+    
+    public string Merged { get; init; } = string.Empty;
+}
