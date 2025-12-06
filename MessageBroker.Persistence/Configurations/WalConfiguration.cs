34dced706218ee0b77c46591976f13e388b6e512
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-06 15:42:18 +0200

    Add file configurations

---
+++ MessageBroker.Persistence/Configurations/WalConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace MessageBroker.Persistence.Configurations;
+
+public record WalConfiguration
+{
+    public string Directory { get; init; } = "/data/wal";
+    public string FileExtension { get; init; } = "log";
+
+    public WalFileBaseNames FileBaseNames { get; init; } = new();
+    public ManifestConfiguration Manifest { get; init; } = new();
+}
