46fc60a3b96e57933925e210cb085849db0133a5
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-07 15:30:43 +0200

    implement CriticalErrorWalDecorator decorator
    - Guarantees graceful shutdown;
    - Enhanced application safety. Blocks subsequent requests once the critical state is confirmed;
    - Add unit tests verifying logic correctness;

---
+++ MessageBroker.Engine/Decorators/Wal/CriticalErrorWalDecorator.cs
@@ -0,0 +1,55 @@
+﻿using MessageBroker.Engine.Abstractions;
+using MessageBroker.Persistence.Abstractions;
+using MessageBroker.Persistence.Common.Exceptions;
+using MessageBroker.Persistence.Events;
+
+namespace MessageBroker.Engine.Decorators.Wal;
+
+public class CriticalErrorWalDecorator(
+    IWriteAheadLog innerWal,
+    ICriticalErrorService criticalErrorService)
+    : IWriteAheadLog
+{
+    private readonly Lock _locker = new();
+
+    private volatile bool _isHealthy = true;
+
+    public void Append(WalEvent evt)
+    {
+        if (!_isHealthy)
+        {
+            throw new WalStorageException("Wal storage can't process events.");
+        }
+
+        try
+        {
+            innerWal.Append(evt);
+        }
+        catch (WalStorageException ex)
+        {
+            HandleCriticalError(ex);
+            throw;
+        }
+    }
+
+    private void HandleCriticalError(Exception ex)
+    {
+        if (!_isHealthy)
+        {
+            return;
+        }
+
+        lock (_locker)
+        {
+            if (!_isHealthy)
+            {
+                return;
+            }
+
+            _isHealthy = false;
+            criticalErrorService.Raise(
+                $"Persistence layer reported critical failure with error '{ex.Message}'",
+                ex);
+        }
+    }
+}
---
+++ MessageBroker.UnitTests/Engine/Decorators/CriticalErrorWalDecoratorTests.cs
@@ -0,0 +1,118 @@
+﻿using MessageBroker.Engine.Abstractions;
+using MessageBroker.Engine.Decorators.Wal;
+using MessageBroker.Persistence.Abstractions;
+using MessageBroker.Persistence.Common.Exceptions;
+using MessageBroker.Persistence.Events;
+using Moq;
+using Shouldly;
+
+namespace MessageBroker.UnitTests.Engine.Decorators;
+
+public class CriticalErrorWalDecoratorTests
+{
+    private readonly Mock<IWriteAheadLog> _innerWalMock;
+    private readonly Mock<ICriticalErrorService> _criticalServiceMock;
+
+    public CriticalErrorWalDecoratorTests()
+    {
+        _innerWalMock = new();
+        _criticalServiceMock = new();
+    }
+
+    [Fact]
+    public void Append_ShouldCallInnerWal_WhenSystemIsHealthy()
+    {
+        // Arrange
+        EnqueueWalEvent evt = new(Guid.CreateVersion7(), [0x01]);
+        CriticalErrorWalDecorator sut = CreateSut();
+
+        // Act
+        sut.Append(evt);
+        
+        // Assert
+        _innerWalMock.Verify(i => i.Append(evt), Times.Once);
+        _criticalServiceMock.Verify(
+            c => c.Raise(It.IsAny<string>(), It.IsAny<Exception>()), 
+            Times.Never);
+    }
+
+    [Fact]
+    public void Append_ThrowsExceptionAndCallsShutdownService_WhenWalStorageExceptionOccurs()
+    {
+        // Arrange
+        EnqueueWalEvent evt = new(Guid.CreateVersion7(), [0x01]);
+        WalStorageException exception = new("Custom exception");
+
+        _innerWalMock.Setup(i => i.Append(evt))
+            .Throws(exception);
+        
+        CriticalErrorWalDecorator sut = CreateSut();
+        
+        // Act
+        Action actual = () => sut.Append(evt);
+        
+        // Assert
+        actual.ShouldThrow<WalStorageException>(exception.Message);
+
+        _criticalServiceMock.Verify(
+            c => c.Raise(It.IsAny<string>(), exception),
+            Times.Once);
+    }
+
+    [Fact]
+    public void Append_DoesNotCallShutdownService_WhenWalStorageExceptionDoesNotOccur()
+    {
+        // Arrange
+        EnqueueWalEvent evt = new(Guid.CreateVersion7(), [0x01]);
+        Exception exception = new("Custom exception");
+
+        _innerWalMock.Setup(i => i.Append(evt))
+            .Throws(exception);
+        
+        CriticalErrorWalDecorator sut = CreateSut();
+        
+        // Act
+        Action actual = () => sut.Append(evt);
+        
+        // Assert
+        actual.ShouldThrow<Exception>(exception.Message);
+
+        _criticalServiceMock.Verify(
+            c => c.Raise(It.IsAny<string>(), It.IsAny<Exception>()),
+            Times.Never);
+    }
+
+    [Fact]
+    public void Append_CallsShutdownServiceOnlyOnce_WhenWalStorageExceptionOccurs()
+    {
+        // Arrange
+        WalStorageException exception = new("Custom exception");
+
+        _innerWalMock.Setup(i => i.Append(It.IsAny<WalEvent>()))
+            .Throws(exception);
+        
+        CriticalErrorWalDecorator sut = CreateSut();
+        
+        // Act
+        Parallel.For(0, 10000, _ =>
+        {
+            try
+            {
+                sut.Append(new EnqueueWalEvent(Guid.CreateVersion7(), [0x01]));
+            }
+            catch { }
+        });
+        
+        // Assert
+        _criticalServiceMock.Verify(
+            c => c.Raise(It.IsAny<string>(), It.IsAny<Exception>()), 
+            Times.Once);
+    }
+
+    private CriticalErrorWalDecorator CreateSut()
+    {
+        return new(
+            _innerWalMock.Object,
+            _criticalServiceMock.Object);
+    }
+}
