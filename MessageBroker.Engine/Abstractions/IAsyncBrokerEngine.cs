9e53e5bd6c67a94734ce48ba06b47aa328c3d691
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-07 18:21:28 +0200

    Implement async broker engine wrapper
    - Introduce IAsyncBrokerEngine interface to enable non-blocking API interactions;

---
+++ MessageBroker.Engine/Abstractions/IAsyncBrokerEngine.cs
@@ -0,0 +1,12 @@
+﻿using MessageBroker.Core.Messages.Models;
+
+namespace MessageBroker.Engine.Abstractions;
+
+public interface IAsyncBrokerEngine
+{
+    Task PublishAsync(byte[] payload);
+    
+    Task<Message?> ConsumeAsync();
+    
+    Task AckAsync(Guid messageId);
+}
