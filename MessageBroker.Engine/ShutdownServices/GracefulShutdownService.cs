c5e5d2c0f883e2beb637d938b1118596fbe64fd8
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-07 13:43:44 +0200

    Implement graceful shutdown service

---
+++ MessageBroker.Engine/ShutdownServices/GracefulShutdownService.cs
@@ -0,0 +1,17 @@
+﻿using MessageBroker.Engine.Abstractions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MessageBroker.Engine.ShutdownServices;
+
+public class GracefulShutdownService(
+    IHostApplicationLifetime appLifetime, 
+    ILogger<GracefulShutdownService> logger) 
+    : ICriticalErrorService
+{
+    public void Raise(string message, Exception exception)
+    {
+        logger.LogCritical(exception, "CRITICAL ERROR: {Message}. Shutting down.", message);
+        appLifetime.StopApplication();
+    }
+}
