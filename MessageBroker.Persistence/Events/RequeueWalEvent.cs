68351688dad8d99228c58faca6364a93741b5767
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-09 20:36:36 +0200

    Add requeue value for WalEventType enum

--- MessageBroker.Persistence/Events/RequeueWalEvent.cs
+++ MessageBroker.Persistence/Events/RequeueWalEvent.cs
@@ -1,4 +1,7 @@
 ﻿namespace MessageBroker.Persistence.Events;
 
-public record RequeueWalEvent(Guid MessageId) 
-    : EnqueueWalEvent(MessageId, []);
+public record RequeueWalEvent(Guid MessageId)
+    : EnqueueWalEvent(MessageId, [])
+{
+    public override WalEventType Type => WalEventType.Requeue;
+}
--- MessageBroker.Persistence/Events/WalEventType.cs
+++ MessageBroker.Persistence/Events/WalEventType.cs
@@ -3,6 +3,7 @@
 public enum WalEventType
 {
     Enqueue,
+    Requeue,
     Ack,
     Dead
 }
