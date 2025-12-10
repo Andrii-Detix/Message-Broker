fa86fe07241bc0ca10441e6d0600fc5c95a85e06
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-10 19:48:55 +0200

    Add extension method to add custom configurations

---
+++ MessageBroker.EndToEndTests/Extensions/WebFactoryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace MessageBroker.EndToEndTests.Extensions;
+
+public static class WebFactoryExtensions
+{
+    extension<T>(WebApplicationFactory<T> factory) 
+        where T : class
+    {
+        public WebApplicationFactory<T> WithOptions(Dictionary<string, string?> settings)
+        {
+            return factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((_, config) =>
+                {
+                    config.AddInMemoryCollection(settings);
+                });
+            });
+        }
+        
+        public WebApplicationFactory<T> WithOption(string key, string value)
+        {
+            return factory.WithOptions(new Dictionary<string, string?> 
+            { 
+                { key, value } 
+            });
+        }
+    }
+}
