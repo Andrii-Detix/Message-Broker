95b702843062dcb2c33a7e23b21089c9e096d818
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-12 23:13:18 +0200

    Add DockerContainerFactory to build test container

---
+++ MessageBroker.EndToEndTests/Abstractions/BrokerContainerFactory.cs
@@ -0,0 +1,41 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+
+namespace MessageBroker.EndToEndTests.Abstractions;
+
+public static class BrokerContainerFactory
+{
+    private const string ImageName = "message-broker:test";
+    private const string MountPoint = "/data";
+    private const string WalDirectory = "/data/wal";
+
+    public const int InternalPort = 8080;
+
+    public static IContainer Create(
+        string? hostDirectory = null,
+        bool resetOnStart = false,
+        Dictionary<string, string>? envVars = null)
+    {
+        ContainerBuilder builder = new ContainerBuilder()
+            .WithImage(ImageName)
+            .WithPortBinding(InternalPort, true)
+            .WithEnvironment("MessageBroker__Wal__Directory", WalDirectory)
+            .WithEnvironment("MessageBroker__Wal__ResetOnStartup", resetOnStart.ToString())
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilInternalTcpPortIsAvailable(InternalPort));
+
+        if (!string.IsNullOrWhiteSpace(hostDirectory))
+        {
+            builder = builder.WithBindMount(hostDirectory, MountPoint);
+        }
+
+        if (envVars is not null)
+        {
+            foreach (var envVar in envVars)
+            {
+                builder = builder.WithEnvironment(envVar.Key, envVar.Value);
+            }
+        }
+        
+        return builder.Build();
+    }
+}
