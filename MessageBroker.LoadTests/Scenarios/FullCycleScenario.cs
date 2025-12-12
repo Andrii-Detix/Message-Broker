e9381bad0bbcb6a57994a6463d657e1a99ace362
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-12 16:23:23 +0200

    Add full cycle load tests

---
+++ MessageBroker.LoadTests/Scenarios/FullCycleScenario.cs
@@ -0,0 +1,43 @@
+﻿using MessageBroker.LoadTests.Helpers;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Data.CSharp;
+
+namespace MessageBroker.LoadTests.Scenarios;
+
+public static class FullCycleScenario
+{
+    public static ScenarioProps Create(HttpClient httpClient, LoadTestConfig config)
+    {
+        var payloads = PayloadGenerator.Generate(10000, config.PayloadMinBytes, config.PayloadMaxBytes);
+        var dataFeed = DataFeed.Circular(payloads);
+
+        return Scenario.Create(config.ScenarioName, async context =>
+            {
+                byte[] payload = dataFeed.GetNextItem(context.ScenarioInfo);
+
+                var pubStep = await BrokerSteps.Publish(context, httpClient, payload);
+                
+                if (pubStep.IsError)
+                {
+                    return pubStep;
+                }
+
+                var consumeStep = await BrokerSteps.Consume(context, httpClient);
+            
+                if (consumeStep.IsError || consumeStep.StatusCode == "204")
+                {
+                    return consumeStep;
+                }
+
+                if (consumeStep.Payload.Value is string messageId)
+                {
+                    return await BrokerSteps.Ack(context, httpClient, messageId);
+                }
+
+                return Response.Fail(message: "Failed to extract Message ID");
+            })
+            .WithWarmUpDuration(config.WarmUp)
+            .WithLoadSimulations(config.Simulations);
+    }
+}
---
+++ MessageBroker.LoadTests/Tests/FullCycleTests.cs
@@ -0,0 +1,91 @@
+﻿using MessageBroker.EndToEndTests.Abstractions;
+using MessageBroker.LoadTests.Abstractions;
+using MessageBroker.LoadTests.Scenarios;
+using NBomber.CSharp;
+using Xunit.Abstractions;
+
+namespace MessageBroker.LoadTests.Tests;
+
+public class FullCycleTests(BrokerFactory factory, ITestOutputHelper output) 
+    : BaseLoadTest(factory, output)
+{
+    [Fact]
+    public void FullCycle_Default_OneClient()
+    {
+        LoadTestConfig config = new()
+        {
+            ScenarioName = "full_cycle_default_one_client",
+            PayloadMinBytes = 1024,
+            PayloadMaxBytes = 4096,
+            WarmUp = TimeSpan.FromSeconds(5),
+            Simulations = 
+            [
+                Simulation.KeepConstant(copies: 1, TimeSpan.FromSeconds(45))
+            ]
+        };
+        
+        RunNBomber(FullCycleScenario.Create(Client, config), config);
+    }
+
+    [Fact]
+    public void FullCycle_Default_MultipleClients()
+    {
+        LoadTestConfig config = new()
+        {
+            ScenarioName = "full_cycle_default_multiple_clients",
+            PayloadMinBytes = 1024,
+            PayloadMaxBytes = 4096,
+            WarmUp = TimeSpan.FromSeconds(5),
+            Simulations = 
+            [
+                Simulation.RampingConstant(copies: 10, TimeSpan.FromSeconds(15)),
+                Simulation.KeepConstant(copies: 10, TimeSpan.FromSeconds(30))
+            ]
+        };
+        
+        RunNBomber(FullCycleScenario.Create(Client, config), config);
+    }
+
+    [Fact]
+    public void FullCycle_LargePayloads()
+    {
+        LoadTestConfig config = new()
+        {
+            ScenarioName = "full_cycle_large_payloads",
+            PayloadMinBytes = 1024 * 90,
+            PayloadMaxBytes = 1024 * 100,
+            WarmUp = TimeSpan.FromSeconds(5),
+            Simulations = 
+            [
+                Simulation.RampingConstant(copies: 10, TimeSpan.FromSeconds(15)),
+                Simulation.KeepConstant(copies: 10, TimeSpan.FromSeconds(30))
+            ]
+        };
+        
+        RunNBomber(FullCycleScenario.Create(Client, config), config);
+    }
+
+    [Fact]
+    public void FullCycle_SoakTest_WithGarbageCollection()
+    {
+        WithOptions(new()
+        {
+            { "MessageBroker:Wal:GarbageCollector:CollectInterval", "00:01:00" }
+        });
+        
+        LoadTestConfig config = new()
+        {
+            ScenarioName = "full_cycle_soak_test_with_garbage_collection",
+            PayloadMinBytes = 1024,
+            PayloadMaxBytes = 4096,
+            WarmUp = TimeSpan.FromSeconds(5),
+            Simulations = 
+            [
+                Simulation.RampingConstant(copies: 10, TimeSpan.FromSeconds(15)),
+                Simulation.KeepConstant(copies: 10, TimeSpan.FromMinutes(5))
+            ]
+        };
+        
+        RunNBomber(FullCycleScenario.Create(Client, config), config);
+    }
+}
