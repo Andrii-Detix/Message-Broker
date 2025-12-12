69a0ad7cbf01af2aacab0e8044cbd94808d5aade
Andrii Ivanchyshyn <aivancisin6@gmail.com>
2025-12-12 13:55:40 +0200

    Add PublishOnly load test scenario
    - Add PublishOnlyScenario static class implementing load test scenario for publishing messages only;
    - Add PublishOnlyTests class to run the scenario under different configurations and load patterns;

---
+++ MessageBroker.LoadTests/Scenarios/PublishOnlyScenario.cs
@@ -0,0 +1,28 @@
+﻿using MessageBroker.LoadTests.Helpers;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Data;
+using NBomber.Data.CSharp;
+
+namespace MessageBroker.LoadTests.Scenarios;
+
+public static class PublishOnlyScenario
+{
+    public static ScenarioProps Create(HttpClient httpClient, LoadTestConfig config)
+    {
+        byte[][] payloads = PayloadGenerator.Generate(10000, config.PayloadMinBytes, config.PayloadMaxBytes);
+
+        IDataFeed<byte[]> dataFeed = DataFeed.Circular(payloads);
+        
+        return Scenario.Create(config.ScenarioName, async context =>
+            {
+                byte[] payload = dataFeed.GetNextItem(context.ScenarioInfo);
+
+                var step = await BrokerSteps.Publish(context, httpClient, payload);
+
+                return step;
+            })
+            .WithWarmUpDuration(config.WarmUp)
+            .WithLoadSimulations(config.Simulations);
+    }
+}
---
+++ MessageBroker.LoadTests/Tests/PublishOnlyTests.cs
@@ -0,0 +1,67 @@
+﻿using MessageBroker.EndToEndTests.Abstractions;
+using MessageBroker.LoadTests.Abstractions;
+using MessageBroker.LoadTests.Scenarios;
+using NBomber.CSharp;
+using Xunit.Abstractions;
+
+namespace MessageBroker.LoadTests.Tests;
+
+public class PublishOnlyTests(BrokerFactory factory, ITestOutputHelper output) 
+    : BaseLoadTest(factory, output)
+{
+    [Fact]
+    public void PublishOnly_Default_OnePublisher()
+    {
+        LoadTestConfig config = new()
+        {
+            ScenarioName = "publish_only_default_one_publisher",
+            PayloadMinBytes = 1024,
+            PayloadMaxBytes = 4096,
+            WarmUp = TimeSpan.FromSeconds(5),
+            Simulations = 
+            [
+                Simulation.KeepConstant(copies: 1, TimeSpan.FromSeconds(45))
+            ]
+        };
+
+        RunNBomber(PublishOnlyScenario.Create(Client, config), config);
+    }
+
+    [Fact]
+    public void PublishOnly_Default_MultiplePublishers()
+    {
+        LoadTestConfig config = new()
+        {
+            ScenarioName = "publish_only_default_multiple_publishers",
+            PayloadMinBytes = 1024,
+            PayloadMaxBytes = 4096,
+            WarmUp = TimeSpan.FromSeconds(5),
+            Simulations = 
+            [
+                Simulation.RampingConstant(copies: 10, TimeSpan.FromSeconds(15)),
+                Simulation.KeepConstant(copies: 10, TimeSpan.FromSeconds(30))
+            ]
+        };
+
+        RunNBomber(PublishOnlyScenario.Create(Client, config), config);
+    }
+
+    [Fact]
+    public void PublishOnly_LargePayloads()
+    {
+        LoadTestConfig config = new()
+        {
+            ScenarioName = "publish_only_large_payloads",
+            PayloadMinBytes = 1024 * 90,
+            PayloadMaxBytes = 1024 * 100,
+            WarmUp = TimeSpan.FromSeconds(5),
+            Simulations = 
+            [
+                Simulation.RampingConstant(copies: 10, TimeSpan.FromSeconds(15)),
+                Simulation.KeepConstant(copies: 10, TimeSpan.FromSeconds(30))
+            ]
+        };
+
+        RunNBomber(PublishOnlyScenario.Create(Client, config), config);
+    }
+}
